5cc7a0d54ca77c30b757aa58e2ea1f5cc44af475
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-06 00:56:17 +0200

    add hash to user registration and login

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -1,30 +1,39 @@
 import {Router} from "express";
 import {UserRecord} from "../records/user.record";
+import {decryptText, encryptText} from "../utils/cipher";
+import {salt} from "../config/config";
 
 export const userRouter = Router()
 
     .post('/user/registration', async (req, res) => {
-        const newUserRegister = await new UserRecord(req.body);
+        const pass = await encryptText(req.body.password, req.body.password, salt);
         const checkUser = await UserRecord.getUser(req.body.name);
         if (checkUser && checkUser.name === req.body.name) {
             res.json({message: 'username is already in use'})
         } else {
-            await newUserRegister.insertUser()
+            const newUserRegister = await new UserRecord({
+                ...req.body,
+                password: pass.encrypted,
+                ivHex: pass.iv,
+            });
+            await newUserRegister.insertUser();
             res.json({message: 'added'})
         }
     })
 
     .post('/', async (req, res) => {
         const data = await UserRecord.getUser(req.body.name);
-
+        let userPassMatch;
         if (!data) {
-            res.json({message: "user not found"})
+            userPassMatch = '';
+            res.json({message: "user not found"});
         } else {
+            userPassMatch = await decryptText(data.password, req.body.password, salt, data.ivHex);
             const simpleUserInfo = {
                 name: data.name,
                 id: data.id,
             }
-            res.json(data.password === req.body.password ? simpleUserInfo : {message: 'login attempt'});
+            res.json(userPassMatch === req.body.password ? simpleUserInfo : {message: 'login attempt'});
         }
     })
 

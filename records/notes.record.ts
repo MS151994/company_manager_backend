6bb7bcd55edf8a6fd3002751f0d2f42601e86510
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-08 08:12:47 +0200

    add notes record

---
+++ records/notes.record.ts
@@ -0,0 +1,60 @@
+import {NewNote, NotesInterface} from "../types/notes";
+import {ValidationError} from "../utils/errors";
+import {pool} from "../utils/db";
+import {FieldPacket} from "mysql2";
+import {v4 as uuid} from 'uuid';
+
+type NewNotesType = [NotesInterface[], FieldPacket[]]
+
+export class NotesRecord implements NotesInterface {
+    id: string;
+    title: string;
+    text: string;
+    createdAt: Date;
+    userId: string | null;
+
+    constructor(obj: NewNote) {
+        if (!obj.title || obj.title.length > 30) {
+            throw new ValidationError('Title cannot be empty and exceed 30 characters')
+        }
+        if (obj.text.length > 1000) {
+            throw  new ValidationError('Note text length max 1000 characters')
+        }
+
+        this.id = obj.id;
+        this.title = obj.title;
+        this.text = obj.text;
+        this.createdAt = obj.createdAt;
+        this.userId = obj.userId;
+    }
+
+    async insertNote(): Promise<void> {
+        if (!this.id) {
+            this.id = uuid();
+        } else {
+            throw new ValidationError('Cannot insert something that is already inserted!')
+        }
+        await pool.execute("INSERT INTO `notes` (`id`,`title`,`text`,`userId`) VALUES(:id,:title,:text,:userId)", this)
+    }
+
+    static async getAllUserNotes(userId: string): Promise<NotesInterface[]> {
+        const [results] = await pool.execute("SELECT * FROM `notes` WHERE `userId`=:userId", {
+            userId,
+        }) as NewNotesType;
+        return results.map(note => new NotesRecord(note));
+    }
+
+    static async getOneNote(id: string): Promise<NotesInterface> {
+        const [result] = await pool.execute("SELECT * FROM `notes` WHERE `id`=:id", {
+            id,
+        }) as NewNotesType;
+        return result.length === 0 ? null : new NotesRecord(result[0]);
+    }
+
+    static async deleteNote(id: string): Promise<void> {
+        await pool.execute("DELETE FROM `notes` WHERE `id`=:id", {
+            id,
+        })
+    }
+
+}

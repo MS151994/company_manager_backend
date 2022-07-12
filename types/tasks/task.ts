0fd2428d5c93ecd7cdff77e1ba1805055c2f30db
Maciek <maciej.stepien1994@gmail.com>
2022-07-12 16:17:29 +0200

    add tasks types

---
+++ types/tasks/task.ts
@@ -0,0 +1,15 @@
+export interface NewTask extends Omit<TaskInterface, "id"> {
+    id?: string;
+}
+
+export interface TaskInterface {
+    id: string;
+    title: string
+    text: string;
+    nip: number;
+    telNumber: number;
+    isDone: boolean | string;
+    createdAt: Date;
+    deadline: Date;
+    userId: string | null;
+}

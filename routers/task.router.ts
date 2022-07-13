9dbe334fbabe72d6863f77e891e70525c77a49b3
Maciek <maciej.stepien1994@gmail.com>
2022-07-13 20:24:02 +0200

    add task router

--- routers/task.router.ts
+++ routers/task.router.ts
@@ -8,5 +8,23 @@ export const TaskRouter = Router()
         const tasks = await TasksRecord.getAllTask();
         const users = await UserRecord.getAllUser();
         const simpleUserInfo = users.map(user => ({userId: user.id, name: user.name}));
-        res.json([tasks,simpleUserInfo])
+        res.json([tasks, simpleUserInfo])
+    })
+
+    .post('/', async (req, res) => {
+        const newTask = await new TasksRecord({
+            ...req.body,
+            createdAt: new Date(),
+            isDone: false,
+            userId: null,
+        })
+        await newTask.insetTask();
+        res.json({message: "ok"})
+    })
+
+    .patch('/one/:taskId', async (req, res) => {
+        console.log(req.params.taskId);
+        console.log("user:", req.body.userId)
+        await TasksRecord.updateUser(req.params.taskId, req.body.userId);
+        res.json({message: 'ok'});
     })

bb394102c7a26b3497408c14f90006cefac37b8b
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-15 02:40:11 +0200

    added getting new tasks

--- routers/home.router.ts
+++ routers/home.router.ts
@@ -6,6 +6,6 @@ export const HomeRouter = Router()
     .get('/:userId', async (req, res) => {
         const dt = new Date().toISOString().slice(0, 10);
         const tasks = await TasksRecord.getAllSimpleInfoTask(req.params.userId, dt);
-        console.log(tasks)
-        res.json(tasks)
+        const newTasks = await TasksRecord.getAllSimpleInfoNewTask(dt);
+        res.json([tasks, newTasks])
     })

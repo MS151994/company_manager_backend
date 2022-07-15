a3fc14c0c467a09effbdf02251c4b8d774ca1003
Maciek <maciej.stepien1994@gmail.com>
2022-07-15 12:38:40 +0200

    added search endpoint

--- routers/home.router.ts
+++ routers/home.router.ts
@@ -9,3 +9,8 @@ export const HomeRouter = Router()
         const newTasks = await TasksRecord.getAllSimpleInfoNewTask(dt);
         res.json([tasks, newTasks])
     })
+
+    .post('/search', async (req, res) => {
+        const foundedItems = await TasksRecord.searchTask(req.body.search);
+        res.json(foundedItems)
+    })
